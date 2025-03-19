beaf543b1bddb590d38796b5cb70c12530dbb2bb
Amber Trujillo <amber@datatecnica.com>
2025-03-19 15:18:39 -0400

    trying to make output directory

--- snakemake.sh
+++ snakemake.sh
@@ -18,10 +18,10 @@ git clone https://github.com/NIH-HPC/snakemake_profile.git
 
 # Pull the containers
 mkdir envs/ # This empty directory is necessary for storing pulled singularity containers
-apptainer pull envs/single_cell_gpu.sif oras://quay.io/adamcatchingdti/single_cell_gpu:0.8
-
 # Making output directory of output docs
 mkdir output/
+apptainer pull envs/single_cell_gpu.sif oras://quay.io/adamcatchingdti/single_cell_gpu:0.8
+
 
 # Load singularity
 module load singularity/4.1.5
